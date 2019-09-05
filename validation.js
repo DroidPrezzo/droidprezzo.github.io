5fff506e58361ab23ebce24ca35161e73a69fb8a
Oluwafemi Precious Sanni <33557670+DroidPrezzo@users.noreply.github.com>
2019-09-05 05:26:11 +0100

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,3 +1,15 @@
 function validation (){
+var name = document.forms["contact"]["name"].value;
+    if (name.length < 4) {
+alert("Please enter at least four characters in the name field")
+            document.myForm.name.focus() ;
+            return false;
+}
+var message = document.forms["contact"]["message"].value;
+    if(message.length < 20){
+alert("Please enter at least twenty characters in the message field")
+            document.myForm.message.focus() ;
+            return false;
+}
 
 }
