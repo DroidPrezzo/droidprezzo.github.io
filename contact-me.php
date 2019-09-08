594f477f902c3219fbc8269e1c257cd15fa6749a
Oluwafemi Precious Sanni <33557670+DroidPrezzo@users.noreply.github.com>
2019-09-08 07:18:12 +0100

    Update contact-me.php

--- contact-me.php
+++ contact-me.php
@@ -6,12 +6,19 @@ $message_field = $_POST['message'];
 $email_field = $_POST['email'];
 $to = "info@prohealthtips.com.ng";
 
+$contactTxt = fopen('message-from-contact.txt', 'a')
+or die('Please try again later! Unable to show your message at this time');
+$content="message from: $name_field \n Email $email_field \n Title: $title_field\nMessage: $message_field";
+fwrite($contactTxt, $content);
+fclose($contactTxt);
 echo"Your Message has been received! I'll get back to you shortly";
 mail($to, $title_field, $content);
 }
 else {
 echo "blarg!";
 }
+
+
 
 
 
